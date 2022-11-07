c8bf6f7e9dabfb75f2640eff7c5d6e0a5b2d6aba
Suraj_Kumar <sk9899189@gmail.com>
2022-11-07 20:30:50 +0530

    Stage 2

--- force-app/main/default/lwc/binding/binding.js
+++ force-app/main/default/lwc/binding/binding.js
@@ -3,8 +3,6 @@ import { LightningElement } from 'lwc';
 export default class Binding extends LightningElement {
       
       message  = 'Hello World';
-      connectedCallback(){
-            console.log('Hello');
-      }
+      
 
 }
